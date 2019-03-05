74f4731e42e811b5a00f8e151c2fa0b6ef8beb5a
Mohd Faraz <mohd.faraz.abc@gmail.com>
2019-03-05 19:56:06 +0000

    LS-5015: gapps.mk: Fixed inclusion of messaging
    Signed-off-by: Mohd Faraz <mohd.faraz.abc@gmail.com>

--- gapps.mk
+++ gapps.mk
@@ -1,16 +1,10 @@
-
-#Messaging
-PRODUCT_PACKAGES += \
-    messaging \
-    WallpaperPicker
-
-
 # GAPPS
 GAPPS_VARIANT := nano
 $(call inherit-product-if-exists, vendor/opengapps/build/opengapps-packages.mk)
 
 GAPPS_PRODUCT_PACKAGE += Chrome Drive Gmail  
+GAPPS_FORCE_MMS_OVERRIDES := true
 GAPPS_FORCE_BROWSER_OVERRIDES := true
-GAPPS_BYPASS_PACKAGE_OVERRIDES := GoogleCamera messaging
+GAPPS_BYPASS_PACKAGE_OVERRIDES := GoogleCamera
 GAPPS_FORCE_PACKAGE_OVERRIDES := true
 GAPPS_EXCLUDED_PACKAGES := GoogleCamera
